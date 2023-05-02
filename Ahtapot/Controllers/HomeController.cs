80ce55d3dcb3fe2f27c568d8dccc322269697feb
muhammedsaidgokkaya <110455828+muhammedsaidgokkaya@users.noreply.github.com>
2023-05-02 22:32:38 +0300

    Kayıtol

--- Ahtapot/Controllers/HomeController.cs
+++ Ahtapot/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ namespace Ahtapot.Controllers
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        Context c = new Context();
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -23,6 +25,18 @@ namespace Ahtapot.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult Kayit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Kayit(User form)
+        {
+            c.Users.Add(form);
+            c.SaveChanges();
+            return RedirectToAction("Index", "Login");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
